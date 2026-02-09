ecafa706905f1438c4b4983eea1fc353ece792d8
Diya202006 <diyakhamkar20@gmail.com>
2026-02-09 10:30:23 +0530

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,45 @@
+class Stack {
+    int stack[] = new int[5];
+    int top = -1;
+
+    void push(int value) {
+        if (top == 4) {
+            System.out.println("Stack Overflow");
+        } else {
+            top++;
+            stack[top] = value;
+            System.out.println(value + " pushed into stack");
+        }
+    }
+
+    void pop() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+        } else {
+            System.out.println(stack[top] + " popped from stack");
+            top--;
+        }
+    }
+
+    void display() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+        } else {
+            System.out.print("Stack elements: ");
+            for (int i = top; i >= 0; i--) {
+                System.out.print(stack[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.display();
+        s.pop();
+        s.display();
+    }
+}
